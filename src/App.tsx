84a0dcf78dac0c4fc3f867d02982a2269f951629
Shiva Kant <shivakantmishra.liangtuang@gmail.com>
2024-02-29 19:20:28 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-interface AppProps {}
-
 interface Todo {
-  id:number;
-  task: string;
+  id: number;
+  title: string;
+  description: string;
 }
 
-function App(props: AppProps) {
+function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [task, setTask] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
 
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem('todos') || '[]') as Todo[];
@@ -24,15 +24,24 @@ function App(props: AppProps) {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
+  const getNextTodoId = () => {
+    const storedId = parseInt(localStorage.getItem('todoId') || '0', 10);
+    const newId = storedId + 1;
+    localStorage.setItem('todoId', newId.toString());
+    return newId;
+  };
+
   const handleAddTodo = () => {
     if (task.trim() !== '') {
-      setTodos([...todos, { id: todos.length + 1, task }]);
+      const newTodo: Todo = { id: getNextTodoId(), title: task, description };
+      setTodos([...todos, newTodo]);
       setTask('');
+      setDescription('');
     }
   };
-  
-  const handleRemoveTodo = (index: number) => {
-    const newTodos = todos.filter((_, i) => i !== index);
+
+  const handleRemoveTodo = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
 
@@ -43,29 +52,33 @@ function App(props: AppProps) {
         <div className="todo-input">
           <input
             type="text"
-            placeholder="Add a new task"
+            placeholder="Add a title"
             value={task}
             onChange={(e: ChangeEvent<HTMLInputElement>) => setTask(e.target.value)}
           />
+          <textarea
+            placeholder="Add a description"
+            value={description}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
+          />
           <button onClick={handleAddTodo}>Add</button>
         </div>
-        
       </header>
       <div className="container">
         <div className="row ">
-        <div className='col-4'><ul className="todo-list">
-           {todos.map((todo, index) => (
-            <li key={index}>
-              {todo.task}
-              <button onClick={() => handleRemoveTodo(index)}> &#x274C;</button>
-            </li>
-          ))}
-
-        </ul></div>
+          <div className="col-4">
+            <ul className="todo-list">
+              {todos.map((todo) => (
+                <li key={todo.id}>
+                  <strong>{todo.title}</strong>
+                  <p>{todo.description}</p>
+                  <button onClick={() => handleRemoveTodo(todo.id)}> &#x274C;</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-     
-      
     </div>
   );
 }
